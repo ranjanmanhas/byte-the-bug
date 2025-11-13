1fe73b09e61aef46c96f8aa729e5623ca79c11d6
rayoo35-code <kimyuong35@gmail.com>
2025-11-13 11:11:32 +0530

    Add files via upload

--- Question.cpp
+++ Question.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <isotream>
 using namespace std;
+
 int main(){
-    int arr[] = {5,3,9,7,9};
-    int max = arr[0];
-    int secondMax = arr[0];
-    for(int i = 1;i<=5; i++){
-        if(arr[i] > max){//
-            secondMax = max;
-            max = arr[i];
-        }
-        else if (arr[i] > secondMax && arr[i] !=max){
-            secondMax= arr[i];
-        }
-    }
-    cout<<"second largest: "<<secondMax<< endl;
-    return 0;
+	int arr[]= { 5,3, 9, 7, 9};
+	int max = arr[0];
+	int secondMax = arr[0];
+
+	for(int i = 0; i< 5 ; i++){
+		if(arr[i)> max ){
+			max = arr[i];
+		}
+		else if 
+			(arr[i] > secondMax && arr[i] != max){
+				secondMax = arr[i];
+
+			}
+	  }
+		cout<<"second largest: "<< secondMax <<endl;
+		return 0;
+	
 }
--- Question1.cpp
+++ Question1.cpp
@@ -1,21 +1,26 @@
-#include <iostream>
+#include < iostream>
 using namespace std;
+
 int main(){
-int a[3][3]={{1,2,3},{2,4,5},{3,5,6}};
-int i,j,flag=0;
-for(i=0; i<3; i++){
-    for(j=0; j<3;j++){
-        if(a[i][j] == a[j][i]){
-            flag = 1;
-            break;
-        }
-    }
-}
-if (flag == 1){
-    cout<<"matrix is not symetric\n";
-}
-else{
-    cout<<"matrix is symetric\n";
-}
-return 0;
+	int arr[3][3] = {
+		{ 1, 2, 3},
+		{2,4,5},
+		{3,5,6}
+	};
+	 int flag = 0;
+	 
+	 for(int i = 0; i< 3; i++){
+		 for( int j = 0; j<i; j++) {
+			  if( arr[i][j] != arr[j][i]){
+			 flag = 1;
+			 break;
+
+		 }
+	 }
+	 }
+if( flag == 1) 
+cout<<" matrix  is  not symmatric \n";
+else 
+cout<<" matrix is symmatric ";
+ retun 0;
 }
