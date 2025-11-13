4c5eb0649901d48cd6f3910a18f7ba98866784fa
V-I-J-A-A-Y <vijaythemaster128@gmail.com>
2025-11-13 11:10:32 +0530

    Add files via upload

--- Question.cpp
+++ Question.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main(){
-    int arr[]={5,3,9,7,9};
+    int arr[] = {5,3,9,7,9};
     int max = arr[0];
-    int secondmax = arr[0];
-    for( int i=1; i<5;i++;){
-        if(array[i]>max){
-            secondmax= max;
-            max = array[i];
-          }  }
-        else{
-            if(arr[i]> secondmax && arr[i] !=max){
-        secondmax = arr[i];
-            }}
-            cout <<"second largest:"<< secondmax <<endl;
-            return 0;
+    int secondMax = arr[0];
+
+    for(int i =1;i<=5;i++){
+        if(arr[i] > max){
+            secondMax = max;
+            max = arr[i];
         }
+        else if(arr[i]>secondMax && arr[i] != max){
+            secondMax = arr[i];
+        }
+    }
+    cout << "Second largest : " << secondMax << endl;
+    return 0;
+}
---
+++ Question1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a[3][3]={
+    {1,2,3},{2,4,5},{3,5,6}
+};
+int i, j , flag = 0;
+for (i = 0; i < 3; i++ ){
+    for(j = 0;j < 3;j++ ){
+        if(a[i][j] == [j][i]){
+            flag = 1;
+            break;
+        }
+    }
+}
+if (flag == 1){
+    cout<< "Matrix is not symmetric  "<< endl;
+
+}
+else {
+    cout<< "Matrix is Symmetric " << endl ;
+}
+
+}
