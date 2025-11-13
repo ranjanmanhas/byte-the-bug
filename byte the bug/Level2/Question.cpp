35ed05e99629f718b40076d2088387cda06cf3a2
sanikshag01 <sanikshagrover01@gmail.com>
2025-11-12 21:36:36 -0800

    Add files via upload

---
+++ byte the bug/Level2/Question.cpp
@@ -0,0 +1,24 @@
+# include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {5,3,9,7,9};
+    int max = arr[0];
+    int secondMax = arr[1];
+
+    for(int i=1; i<5; i++)
+     {
+        if (arr[i] > max) 
+        {
+            secondMax = max;
+            max = arr[i];
+        } else if (arr[i] > secondMax && arr[i] != max)
+         {
+            secondMax = -1;
+        }
+    }
+
+    cout << "Second largest: " << secondMax << endl;
+    return 0
+}
---
+++ byte the bug/Level3/Question1.cpp
@@ -0,0 +1,32 @@
+#include <iostream.h>
+using namespace std;
+
+int main() {
+    int a[3] [3] = {
+        {1,2,3} , 
+        {2,4,5} ,
+        {3,5,6}
+    };
+    int i, j, flag=0;
+
+    for (i=0; i<3; i++)
+     {
+        for (j = 0; j<3; j++) 
+        {
+            if (a[i] [j] != a[j][i]) 
+            {
+                flag = 1;
+                break;
+            }
+        }
+    }
+    if (flag == 1)
+    {
+        cout<< " MAtrix is not symmetric\n ";
+        
+    } else {
+        
+        cout << " matrix is symmetric\n";
+    }
+      return 0;
+}
