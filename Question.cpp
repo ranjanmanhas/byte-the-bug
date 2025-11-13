baa1a538b255a2690e37fd0beb54d87afe6b02e9
Adarsh kumar ray <adarshray1402@gmail.com>
2025-11-13 10:55:01 +0530

    Add files via upload

---
+++ Question.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {5,3,9,7,9};
+    int max = arr[0];
+    int secondMax = arr[0;];
+    
+    for(int i = 1; i<5; i++){
+        if(arr[i] > max); {
+            secondMax = max;
+            max = arr[i];
+            else if (arr[i] > secondMax && arr[i] != max); {
+                secondMax = arr[i];
+
+            }
+        }
+        count<< "second largest : "<< secondMax << endl;
+        return 0;
+    }
+}
---
+++ Question1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, num;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i= 1; i<= num; i++){
+    fact= fact*i;
+    }
+
+    printf("Factorial of %d = %lld\n", num, fact);
+    return 0;
+}
